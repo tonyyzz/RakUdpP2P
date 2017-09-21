a372ea72058515fe8d8719e67261c1ca86eeb364
yzz <1277745281@qq.com>
2017-09-21 11:36:58 +0800

    -

---
+++ RakUdpP2P/RakUdpP2P.BaseCommon/RaknetMng/RaknetUdpProxy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RakUdpP2P.BaseCommon.RaknetMng
+{
+	/// <summary>
+	/// Raknet统一代理类（综合ProxyCoordinator和ProxyServer）
+	/// </summary>
+	public class RaknetUdpProxy
+	{
+		public RaknetUdpProxy()
+		{
+
+		}
+		public void Start()
+		{
+
+		}
+		public void Stop()
+		{
+
+		}
+	}
+}
